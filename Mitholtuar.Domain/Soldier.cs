281896b62b7b124bcd88847ef74c1d0e7feb13bc
Bret Hayes <bret_hayes@comcast.net>
2019-04-03 07:51:59 -0400

    Using inheritance to define soldier, created basis for armies

---
+++ Mitholtuar.Domain/Soldier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mitholtuar.Domain
+{
+    class Soldier
+    {
+        // virtual health, base health
+        // virtual attack power, base dmg
+        // virtual armor value, base armor value
+        // virtual speed, base speed
+    }
+}
